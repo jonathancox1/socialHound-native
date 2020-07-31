81ce138fc898b1cf43c5b2dd4e1ef1fb954dfa17
Jonathan Cox <cox.jonathan@gmail.com>
2020-07-31 13:07:28 -0400

    init

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Linking, TouchableOpacity } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import sh from './assets/sh-1.jpg'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={['#2096ff', '#05ffa3']} style={styles.linearGradient}>
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => Linking.openURL('https://socialHound.co')}
+        >
+          <Image
+            source={sh}
+            style={styles.img}
+            resizeMode={'contain'}
+          />
+        </TouchableOpacity>
+        <Text style={styles.title}>mobile v1.0</Text>
+        <StatusBar style="auto" />
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
+    alignItems: 'stretch',
     justifyContent: 'center',
+  },
+  title: {
+    position: 'absolute',
+    flexDirection: 'row',
+    top: 530,
+    left: 150,
+    fontSize: 20,
+    height: 100,
+  },
+  img: {
+    maxWidth: 300,
+    alignSelf: 'center',
+    backgroundColor: '#00000000',
   },
 });
---
+++ readme.md
@@ -0,0 +1,11 @@
+will need
+- xcode
+- expo
+
+install dependencies
+- yarn install
+
+to run
+- yarn ios
+- yarn android 
+- yarn web
